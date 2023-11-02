fe00aa6dddd0e80607db8c2647644ecebfce7e18
Scott Campbell <scott@josephscottcampbell.com>
2023-11-02 13:18:24 -0400

    added basic interrupt capability

--- float-side/firmware/firmware.ino
+++ float-side/firmware/firmware.ino
@@ -2,18 +2,41 @@
 #include <ArduinoWebsockets.h>
 #include <WiFi.h>
 
-const char* ssid = "TP-Link_51CA"; // put SSID here
-const char* password = "password"; // put password here
+#define PHASE 9
+#define ENABLE 8
+#define HALL_EFFECT 10
 
-using namespace websockets;
+const char* ssid = "TP-Link_51CA";  // put SSID here
+const char* password = "password";  // put password here
 
-WebsocketsServer server;
+hw_timer_t* recording_timer = NULL;
+unsigned short depth_data[1000];  // array where depth data will be kept
+uint8_t recording_cycle = 0;      // keep track of what index to write to
 
-void profile(){
-  // this will do a vertical profile
+void IRAM_ATTR record_depth() {
+  // get depth data
+  // still waiting for proper cable to arrive
+  // placeholderch
+  if (recording_cycle > 999) {  // preventing buffer overflow
+    recording_cycle = 0;
+  }
+  depth_data[recording_cycle] = recording_cycle;
 }
 
+using namespace websockets;
+WebsocketsServer server;
+
 void setup() {
+  // setting up interupts
+  // running record_depth() every 5 seconds
+  recording_timer = timerBegin(0, 80, true);
+  timerAttachInterrupt(recording_timer, &record_depth, true);
+  timerAlarmWrite(recording_timer, 1000000, true);
+  timerAlarmEnable(recording_timer);
+  // Setting up GPIO
+  pinMode(ENABLE, OUTPUT);
+  pinMode(PHASE, OUTPUT);
+
   Serial.begin(115200);
   // Connect to wifi
   WiFi.begin(ssid, password);
@@ -27,16 +50,17 @@ void setup() {
   Serial.println("");
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());   //You can get IP address assigned to ESP
+  Serial.println(WiFi.localIP());  //You can get IP address assigned to ESP
 
   server.listen(80);
   Serial.print("Is server live? ");
   Serial.println(server.available());
 }
 
+
 void loop() {
-  bool looping = true; // allows clean break from loop
-  WebsocketsClient client = server.accept(); // accepting websockets connection
+  bool looping = true;                        // allows clean break from loop
+  WebsocketsClient client = server.accept();  // accepting websockets connection
   if (client.available()) {
     while (looping) {
       WebsocketsMessage msg = client.readBlocking();
@@ -48,9 +72,20 @@ void loop() {
       // uses if statement because switch statements want chars
       if (msg.data() == "profile") {
         client.send("going down");
+        looping = false;
+        client.close();  // closing client because connection will time out once it goes underwater
+        // profile();
       } else if (msg.data() == "break") {
         client.send("goodbye");
         looping = false;
+      } else if (msg.data() == "get_data") {
+        String message = "[";
+        for (int i = 0; i < recording_cycle; i++) {
+          message = message + depth_data[recording_cycle];
+          message = message + ", ";
+        }
+        message = message + "]";
+        client.send(message);
       } else {
         client.send(msg.data());
       }
@@ -58,4 +93,8 @@ void loop() {
     // close the connection
     client.close();
   }
+}
+
+void profile() {
+  // placeholder
 }
